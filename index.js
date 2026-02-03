8852e0f8e1ba126f61b76d17cc48132a4c83a3b9
Greta Ra <ggretute88@gmail.com>
2026-02-03 16:53:00 +0100

    Dynamic shopping cart display and add/remove items

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ import { menuArray } from "./data.js";
 
 const menuEl = document.getElementById('menu')
 
+let shoppingCart = []
+
+document.addEventListener('click', function(e){
+   if(e.target.classList.contains("add-btn")){
+      const id = Number(e.target.dataset.id)
+      addToCart(id)
+   } else if (e.target.classList.contains("remove-btn")){
+      const id = Number(e.target.dataset.id)
+      removeItem(id)
+   }
+})
+
 function createMenu (){
   let menuList = ``
 
@@ -9,15 +21,15 @@ function createMenu (){
     menuList += `
       <div class="menu-item">
         <div class="item-emoji">${item.emoji}</div>
-        <div class="item-info" id="${item.id}">
+        <div class="item-info" data-id="${item.id}">
             <h3 class="item-name">${item.name}</h3>
             <p class="item-ingredients">${item.ingredients.join(', ')}</p>
             <p class="price">$${item.price}</p>
         </div>
-        <button class="add-btn">+</button>
+        <button class="add-btn" data-id="${item.id}">+</button>
       </div>
     `
-  });
+  })
 
   return menuList
 }
@@ -27,3 +39,46 @@ function renderMenu (){
 }
 
 renderMenu()
+
+function addToCart (selectedItemId){
+
+  const item = menuArray.find( i => i.id === selectedItemId)
+  
+  shoppingCart.push(item)
+
+  renderShoppingCart(shoppingCart)
+}
+
+function removeItem (selectedItemId){
+  const item = menuArray.find( i => i.id === selectedItemId)
+
+  shoppingCart.splice(shoppingCart.indexOf(item), 1)
+
+  renderShoppingCart(shoppingCart)
+}
+
+function renderShoppingCart (cart) {
+  const orderSummaryEl = document.getElementById('order-summary')
+
+  if(shoppingCart.length > 0){
+    orderSummaryEl.style.display = 'block'
+  } else{
+    orderSummaryEl.style.display = 'none'
+  }
+
+  let cartHtml = ``
+
+  cart.map((item) => {
+    cartHtml += `
+      <li class="summary-item">
+        <h3 class="item-name">${item.name}</h3>
+        <div class="remove-btn-container">
+            <button class="remove-btn type="button">remove</button>
+        </div>
+        <p class="price">$${item.price}</p>
+    </li>
+    `
+  })
+
+  document.getElementById('summary-item-list').innerHTML = cartHtml
+}
